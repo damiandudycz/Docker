94414f3f2de2bb96fc4084c646679d118c6f3383
Damian Dudycz <damiandudycz@yahoo.com>
2022-12-18 11:47:51 +0100

    Changes in script

--- linux-bootstrap.sh
+++ linux-bootstrap.sh
@@ -70,38 +70,50 @@ if [ "$EUID" -ne 0 ]; then
     echo "Root privileges are required to run this script"; exit
 fi
 if [ -z "$DEVICE" ] || [ ! -e "$DEVICE" ]; then
-    echo "Invalid device. The specified device does not exist or is not a block device."; exit
+    echo "Invalid device. The specified device does not exist or is not a "\
+    "block device."; exit
 fi
 if [ "$(ls -A "$MOUNTPOINT")" ]; then
-    echo "The mountpoint directory is not empty. Please specify an empty directory as the mountpoint."; exit
+    echo "The mountpoint directory is not empty. Please specify an empty "\
+    "directory as the mountpoint."; exit
 fi
 if ! [[ "$NAME" =~ ^[a-zA-Z0-9]*$ ]]; then
-    echo "Invalid hostname. The hostname can only contain alphanumeric characters."; exit
+    echo "Invalid hostname. The hostname can only contain alphanumeric "\
+    "characters."; exit
 fi
 if [ -z "$TIMEZONE" ] || [ ! -f "/usr/share/zoneinfo/$TIMEZONE" ]; then
-    echo "Invalid timezone. Please specify a valid timezone from the list in /usr/share/zoneinfo."; exit
+    echo "Invalid timezone. Please specify a valid timezone from the list in "\
+    "/usr/share/zoneinfo."; exit
 fi
 if [ -z "$LOCALE" ] || [[ "$(locale -a | grep -w "$LOCALE")" != "" ]]; then
     echo "Invalid locale. The specified locale does not exist."; exit
 fi
-if [ "$FIRMWARE" != "efi" ] && [ "$FIRMWARE" != "bios" ] && [ "$FIRMWARE" != "none" ]; then
-    echo "Error: Invalid value for the --firmware parameter. Must be one of: efi, bios, none."; exit
+if [ "$FIRMWARE" != "efi" ] && [ "$FIRMWARE" != "bios" ]\
+&& [ "$FIRMWARE" != "none" ]; then
+    echo "Error: Invalid value for the --firmware parameter. Must be one of: "\
+    "efi, bios, none."; exit
 fi
 if [ "$ROOTFS" != "ext4" ] && [ "$ROOTFS" != "btrfs" ]; then
-      echo "Error: Invalid value for the --rootfs parameter. Must be either ext4 or btrfs."; exit
+      echo "Error: Invalid value for the --rootfs parameter. Must be either "\
+      "ext4 or btrfs."; exit
 fi
 if [ "$BOOTFS" != "vfat" ] && [ "$BOOTFS" != "ext4" ]; then
-      echo "Error: Invalid value for the --bootfs parameter. Must be either vfat or ext4."; exit
+      echo "Error: Invalid value for the --bootfs parameter. Must be either "\
+      "vfat or ext4."; exit
 fi
 if [ "$FIRMWARE" == "efi" ] && [ "$BOOTFS" != "vfat" ]; then
-    echo "Error: When firmware is set to EFI, boot filesystem must be set to vfat."; exit
+    echo "Error: When firmware is set to EFI, boot filesystem must be set to "\
+    "vfat."; exit
 fi
 
 # -----------------------------------------------------------------------------
 # PREPARING DEVICE ------------------------------------------------------------
 
 # wipe disk space and create disk layout
-dd if=/dev/zero of=$DEVICE bs=10M count=$(blockdev --getsize64 /dev/sda | awk '{print $1/512}') status=progress 2>&1
+dd if=/dev/zero of=$DEVICE bs=10M\
+count=$(blockdev --getsize64 /dev/sda | awk '{print $1/512}')\
+status=progress 2>&1
+
 FDINIT="g\n" # Create GPT table
 FDBOOT="n\n1\n\n+128M\n" # Add boot partition
 FDROOT="n\n2\n\n\n" # Add root partition
@@ -137,8 +149,11 @@ mount "$BOOTDEV" "$MOUNTPOINT/boot"
 # -----------------------------------------------------------------------------
 # BOOTSTRAPING ----------------------------------------------------------------
 
-# Pobranie tekstu z podanego URL i wyciągnięcie z niego informacji o ścieżce do pliku stage3 i jego rozmiarze
-STAGE3_PATH_SIZE=$(curl -L https://gentoo.osuosl.org/releases/amd64/autobuilds/latest-stage3-amd64-openrc.txt | grep -v '^#')
+# Pobranie tekstu z podanego URL i wyciągnięcie z niego informacji o ścieżce
+# do pliku stage3 i jego rozmiarze
+STAGE3_DETAILS_URL="https://gentoo.osuosl.org/releases/amd64/autobuilds/"\
+"latest-stage3-amd64-openrc.txt"
+STAGE3_PATH_SIZE=$(curl -L $STAGE3_DETAILS_URL | grep -v '^#')
 STAGE3_PATH=$(echo $STAGE3_PATH_SIZE | cut -d ' ' -f 1)
 
 # Wygenerowanie pełnej ścieżki do pliku tar.xz podanego w pliku tekstowym
@@ -148,8 +163,10 @@ STAGE3_URL="https://gentoo.osuosl.org/releases/amd64/autobuilds/$STAGE3_PATH"
 curl -L "$STAGE3_URL" -o "$MOUNTPOINT/stage3.tar.xz"
 
 # extract the tarball to the root partition
-tar xpvf "$MOUNTPOINT/stage3.tar.xz" --xattrs-include='*.*' --numeric-owner -C "$MOUNTPOINT"
-sed -i 's/^COMMON_FLAGS="/COMMON_FLAGS="-march=native /' "${MOUNTPOINT}/etc/portage/make.conf"
+tar xpvf "$MOUNTPOINT/stage3.tar.xz" --xattrs-include='*.*' --numeric-owner\
+-C "$MOUNTPOINT"
+sed -i 's/^COMMON_FLAGS="/COMMON_FLAGS="-march=native /'\
+"${MOUNTPOINT}/etc/portage/make.conf"
 
 # MAKEOPTS
 echo 'MAKEOPTS="-j4"' >> "${MOUNTPOINT}/etc/portage/make.conf"
@@ -157,7 +174,8 @@ echo 'ACCEPT_LICENSE="*"' >> "${MOUNTPOINT}/etc/portage/make.conf"
 
 # Gentoo ebuild repository
 mkdir --parents "${MOUNTPOINT}/etc/portage/repos.conf"
-cp "${MOUNTPOINT}/usr/share/portage/config/repos.conf" "${MOUNTPOINT}/etc/portage/repos.conf/gentoo.conf"
+cp "${MOUNTPOINT}/usr/share/portage/config/repos.conf"\
+"${MOUNTPOINT}/etc/portage/repos.conf/gentoo.conf"
 
 # Copy DNS info
 cp --dereference /etc/resolv.conf "${MOUNTPOINT}/etc/"
@@ -177,59 +195,58 @@ mount --make-slave ${MOUNTPOINT}/run
 # CHROOT AND SETUP ------------------------------------------------------------
 
 echo "
-    source /etc/profile
-    export PS1=\"(chroot) \${PS1}\"
+source /etc/profile
+export PS1=\"(chroot) \${PS1}\"
 
-    # Setup hostname
-    echo \"$NAME\" >> /etc/conf.d/hostname # For OpenRC
+# Setup hostname
+echo \"$NAME\" >> /etc/conf.d/hostname # For OpenRC
 
-    # Update repository
-    emerge-webrsync
-    emerge --sync --quiet
+# Update repository
+emerge-webrsync
+emerge --sync --quiet
 
-    # Setup CPU flags
-    emerge app-portage/cpuid2cpuflags
-    echo \"*/* \$(cpuid2cpuflags)\" > /etc/portage/package.use/00cpu-flags
+# Setup CPU flags
+emerge app-portage/cpuid2cpuflags
+echo \"*/* \$(cpuid2cpuflags)\" > /etc/portage/package.use/00cpu-flags
 
-    # Update packages
-    emerge --verbose --update --deep --newuse @system
-    emerge --verbose --update --deep --newuse @world
-    
-    # Setup timezone
-    echo $TIMEZONE > /etc/timezone
-    emerge --config sys-libs/timezone-data
+# Update packages
+emerge --verbose --update --deep --newuse @system
+emerge --verbose --update --deep --newuse @world
 
-    # Setup locale
-    sed -i \"/$LOCALE/s/^#//g\" /etc/locale.gen
-    locale-gen
-    locale_num=\$(eselect locale list | grep -i ${LOCALE//-} | awk '/\\]/ {print \$1}' | grep -oP '\\[\\K[^]]+')
-    eselect locale set \$locale_num
-    
-    # Install kernel
-    emerge sys-kernel/gentoo-kernel-bin
-    
-    # Clean
-    emerge --depclean
-    
-    # FSTab
-    echo \"$BOOTDEV /boot   $BOOTFS   defaults,noatime    0 2\" >> /etc/fstab
-    echo \"$ROOTDEV /   $ROOTFS   noatime    0 1\" >> /etc/fstab
+# Setup timezone
+echo $TIMEZONE > /etc/timezone
+emerge --config sys-libs/timezone-data
 
-    # Update env
-    env-update && source /etc/profile && export PS1=\"(chroot) \${PS1}\"
-    
-    # DHCPCD
-    emerge net-misc/dhcpcd
-    rc-update add dhcpcd default
-    
-    sed -i 's/clock=.*/clock=\"local\"/' /etc/conf.d/hwclock
-    
-    # GRUB
-    echo 'GRUB_PLATFORMS=\"efi-64\"' >> /etc/portage/make.conf
-    emerge sys-boot/grub
-    grub-install --target=x86_64-efi --efi-directory=/boot
-    
-    
+# Setup locale
+sed -i \"/$LOCALE/s/^#//g\" /etc/locale.gen
+locale-gen
+locale_num=\$(eselect locale list | grep -i ${LOCALE//-} | awk '/\\]/ "
+\\"{print \$1}' | grep -oP '\\[\\K[^]]+')
+eselect locale set \$locale_num
+
+# Install kernel
+emerge sys-kernel/gentoo-kernel-bin
+
+# Clean
+emerge --depclean
+
+# FSTab
+echo \"$BOOTDEV /boot   $BOOTFS   defaults,noatime    0 2\" >> /etc/fstab
+echo \"$ROOTDEV /   $ROOTFS   noatime    0 1\" >> /etc/fstab
+
+# Update env
+env-update && source /etc/profile && export PS1=\"(chroot) \${PS1}\"
+
+# DHCPCD
+emerge net-misc/dhcpcd
+rc-update add dhcpcd default
+
+sed -i 's/clock=.*/clock=\"local\"/' /etc/conf.d/hwclock
+
+# GRUB
+echo 'GRUB_PLATFORMS=\"efi-64\"' >> /etc/portage/make.conf
+emerge sys-boot/grub
+grub-install --target=x86_64-efi --efi-directory=/boot
 " > $MOUNTPOINT/setup.sh
 chmod +x $MOUNTPOINT/setup.sh
 chroot $MOUNTPOINT /setup.sh
